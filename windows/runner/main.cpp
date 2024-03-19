a32389882c08cfc0f936e725295545a69ed25974
kote_kagbe <kote.kagbe@gmail.com>
2024-03-19 20:16:37 +0300

    windows app name and pos

--- windows/runner/main.cpp
+++ windows/runner/main.cpp
@@ -25,9 +25,9 @@ int APIENTRY wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev,
   project.set_dart_entrypoint_arguments(std::move(command_line_arguments));
 
   FlutterWindow window(project);
-  Win32Window::Point origin(10, 10);
-  Win32Window::Size size(1280, 720);
-  if (!window.Create(L"ez_backend", origin, size)) {
+  Win32Window::Point origin(500, 200);
+  Win32Window::Size size(700, 900);
+  if (!window.Create(L"EZ Backend", origin, size)) {
     return EXIT_FAILURE;
   }
   window.SetQuitOnClose(true);
