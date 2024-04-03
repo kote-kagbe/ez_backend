48960b7ebfc9c2206d411fdac7017f221be2497a
efimovvp <efimovvp@tensor.ru>
2024-04-03 17:08:43 +0300

    colors

--- lib/ui/constants/colors.dart
+++ lib/ui/constants/colors.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
 
 const mainBackground = Color(0xfff8f8f8);
+const mainElementBackgound = Color(0xffffffff);
+const mainElementHover = Color(0xfff3f4f8);
 const mainText = Colors.black;
 
 const sideMenuBackground = Color(0xff2c3e50);
-const sideMenuText = Color(0xffe9ebec);
+const sideMenuItemHover = Color(0xff3e5266);
+const sideMenuItemText = Color(0xffe9ebec);
+const sideMenuItemAddText = Color(0xff98b2ca);
 
 const highlight = Color(0xffff9c6c);
