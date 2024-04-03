35f04232b663515c322144896d61ecad6a278e2a
efimovvp <efimovvp@tensor.ru>
2024-04-03 16:04:06 +0300

    colors

--- lib/ui/constants/colors.dart
+++ lib/ui/constants/colors.dart
@@ -1,5 +1,9 @@
-import 'dart:ui';
+import 'package:flutter/material.dart';
 
-const background = Color(0xfff8f8f8);
-const highlight = Color(0xffff9c6c);
+const mainBackground = Color(0xfff8f8f8);
+const mainText = Colors.black;
+
 const sideMenuBackground = Color(0xff2c3e50);
+const sideMenuText = Color(0xffe9ebec);
+
+const highlight = Color(0xffff9c6c);
